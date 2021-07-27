5e2a2871ffbee7c79f2d95ba05dae6e818ee9075
mohammad <m.zamani1812@gmail.com>
2021-07-27 11:41:49 +0430

    test for learning git

--- server.cpp
+++ server.cpp
@@ -8,7 +8,7 @@ int main(void)
   httplib::Server svr;
 
   svr.Get("/hi", [](const auto &, auto &res)
-          { res.set_content("how many players we have?", "text/plain"); });
+          { res.set_content("how many players we have?\nmake a my app", "text/plain"); });
 
   std::cout << "start server..." << std::endl;
   svr.listen("0.0.0.0", 8080);
